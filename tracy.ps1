2f065a4b8c3df1b488b9cc5cd5cc77da401a21a6
Greeble <166992735+greeble-dev@users.noreply.github.com>
2024-12-20 12:40:02 +0000

    Improved Tracy script. Added postfix, use the target folder instead of temp, and wait for the capture process to exit before starting the UI.

--- tracy.ps1
+++ tracy.ps1
@@ -1,5 +1,24 @@
+# Runs a Tracy capture on an example and then starts the Tracy UI.
+#
+# Usage: tracey.ps1 <example> <optional_postfix>
+#
+# The file is saved to target/<example>_<optional_postfix>.tracy.
+# 
+# TODO: Pass remaining arguments to example?
+
 $example = $args[0]
-Start-Job -ScriptBlock {tracy-capture -f -o $env:Temp/skinned_aabb.tracy}
-cargo run --profile=bench --example=$example --features bevy/trace_tracy
-tracy-profiler $env:Temp/skinned_aabb.tracy
+$postfix = ""
+
+if ( $args.count -gt 1 )
+{
+	$postfix = "_" + $args[1]
+}
+
+$file = "${pwd}/target/${example}${postfix}.tracy"
+
+Start-Job -ScriptBlock { tracy-capture -f -o $using:file }
+cargo run --example=$example --profile=bench --features bevy/trace_tracy --features trace
+Get-Job | Wait-Job
+echo "Profile saved to `"${file}`""
+tracy-profiler $file
 
